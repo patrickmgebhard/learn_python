e88c17ead7b838a7d60284e5e2fe1fc21a33075b
patrickmgebhard <patrickmgebhard@gmail.com>
2022-11-29 22:07:41 +0100

    looping over lists of tuples

--- for_loop.py
+++ for_loop.py
@@ -1,6 +1,9 @@
 fruits = ["apple", "banana", "cherry"]
 adj = ["red", "big", "tasty"]
 
+"""mini_batches = [training_data[k:k+mini_batch_size] for k in xrange(0, n, mini_batch_size)]"""     
+training_data = [(1, 2), (3, 4), (5, 6)]
+
 for x in fruits:
     print(x)
 
@@ -14,5 +17,16 @@ for x in adj:
     for y in fruits:
         print(x, y)
 
+print(zip(adj, fruits))
+
 for x, y in zip(adj, fruits):
+    print(x, y)
+
+for x in training_data:
+    print(x)
+
+for x, y in training_data:
+    print(y)
+
+for x, y in training_data:
     print(x, y)
