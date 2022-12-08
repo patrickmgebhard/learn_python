b658e26ec5e4da9b9458f618cb91147797198621
patrickmgebhard <patrickmgebhard@gmail.com>
2022-12-08 19:57:57 +0100

    learning regex in python

--- numpy_basics.py
+++ numpy_basics.py
@@ -23,8 +23,8 @@ g = np.random.randint(100)
 print(g)
 
 
-import matplotlib.pyplot as plt
-"""import seaborn as sns
+"""import matplotlib.pyplot as plt
+import seaborn as sns
 
 sns.distplot([0, 1, 2, 3, 4, 5])
 
