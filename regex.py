5f8f0feadeeadb55acf848ba5d410613ebf2a282
patrickmgebhard <patrickmgebhard@gmail.com>
2022-12-11 14:28:38 +0100

    Learning more about regex

--- regex.py
+++ regex.py
@@ -2,9 +2,10 @@ import re
 
 txt = "The rain in Spain"
 x = re.search("^The.*Spain$", txt)
-
-y = re.findall("ai", txt)
-
 print(x)
 
+y = re.findall("ai", txt)
 print(y)
+
+z = re.search("\s", txt)
+print(z)
