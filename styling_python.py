ad057772ddfc98c23c8449f9d554560237faf873
Patrick Gebhard <patrickmgebhard@gmail.com>
2023-01-03 23:08:52 +0100

    noting down some python styling rules

--- styling_python.py
+++ styling_python.py
@@ -2,3 +2,17 @@
 -https://peps.python.org/pep-0008/
 -use https://pycodestyle.pycqa.org/en/latest/ to check if you follow the rules
 """
+
+# Naming conventions
+# Package and module names should be short, all-lowercase
+# Examples: pandas, numpy, matplotlib, math
+
+# Class Names should be CapWords
+# Examples: BaseModel, Random
+
+# Function and variable names should be lowercase with words separated by underscores
+# Examples: sort_names
+
+# Constants should be all capital letters with underscores separating words
+# MAX_OVERFLOW, TOTAL
+
