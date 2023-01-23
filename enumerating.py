889887c08131743478d43b6b9d3ad63d2d6cc492
Patrick Gebhard <patrickmgebhard@gmail.com>
2023-01-23 16:54:14 +0100

    learning even more about enumerating

--- enumerating.py
+++ enumerating.py
@@ -9,3 +9,13 @@ values = [10, 9, 8]
 for count, value in enumerate(values):
     print(count, value)
 
+# the start parameters allows the change the starting counter
+for count, value in enumerate(values, start=5):
+    print(count, value)
+
+# use enumerate to fill in a dictionary
+weekdays = {
+    s: i+1 for i, s in enumerate(['Mon', 'Tue', 'Wed', 'Thu', 'Fr', 'Sat', 'Sun'])    
+}
+
+print(weekdays)
